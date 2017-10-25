8818ffe222f2c486cbc3e913e2eb6f436c5c3a95
R-a-i-s-o-n <pawel.urbanczyk90@gmail.com>
2017-10-25 17:43:18 +0200

    Dodanie pliku z ćwieczeniami

---
+++ ciasteczka.php
@@ -0,0 +1,36 @@
+﻿<?
+/*
+if(!isSet($_COOKIE['odwiedziny'])){
+$odw=$_COOKIE['odwiedziny']=1;
+	
+}else{
+$odw=$_COOKIE['odwiedziny']+1;
+   
+}
+
+
+setcookie('odwiedziny', $odw);
+
+echo('<pre>');
+//print_r($_COOKIE);
+echo('<pre>');
+echo('Jesteś u nas po raz: '.$odw);
+*/
+
+if(!isSet($_COOKIE['odwiedziny'])){
+$odw=$_COOKIE['odwiedziny']=1;
+	
+}else{
+$odw=$_COOKIE['odwiedziny']+1;
+   
+}
+
+
+setcookie('odwiedziny', $odw);
+
+echo('<pre>');
+//print_r($_COOKIE);
+echo('<pre>');
+echo('Jesteś u nas po raz: '.$odw);
+
+?>
